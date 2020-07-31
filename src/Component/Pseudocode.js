013af038e47fc8714c5d785c4abe8a3107b9c74e
sindhubedare <sindhubedare1990@gmail.com>
2020-07-31 12:15:44 -0500

    added final steps

--- src/Component/Pseudocode.js
+++ src/Component/Pseudocode.js
@@ -3,3 +3,4 @@
 //have a click button
 //connect that button to toggle the changing color
 //on click of the button, the number of counts should be counted using clickcounter
+//commit the final code
